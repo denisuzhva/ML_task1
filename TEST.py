1ad89f972831091b4a758cb8e9f8829aaa27003e
mortarsynth <epirtochol@gmail.com>
2019-09-15 12:17:51 +0300

    done

--- TEST.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import csv
-
-
-with open('./Dataset/Features_TestSet.csv', 'r') as dataset_csv:
-    dataset_list = list(csv.reader(dataset_csv, delimiter=','))
-dataset_np = np.array(dataset_list, dtype=np.float)
-
-print(dataset_np.shape)
